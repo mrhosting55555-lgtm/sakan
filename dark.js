b6319d9bb3ad792acc3433b67ae0324eb956299f
wahep tamer <mrhosting55555@gmail.com>
2026-06-11 15:45:38 +0300

    update site

--- dark.js
+++ dark.js
@@ -1,19 +1,12 @@
-function toggleTheme() {
-  document.body.classList.toggle("dark");
-
-  if (document.body.classList.contains("dark")) {
-    localStorage.setItem("theme", "dark");
-  } else {
-    localStorage.setItem("theme", "light");
-  }
-}
-
-function applyTheme() {
-  const theme = localStorage.getItem("theme");
-
-  if (theme === "dark") {
-    document.body.classList.add("dark");
-  }
-}
-
-document.addEventListener("DOMContentLoaded", applyTheme);
+// تحديد الزرار وجسم الصفحة
+const themeToggleBtn = document.querySelector('.theme-toggle'); 
+const body = document.body;
+themeToggleBtn.addEventListener('click', () => {
+    if (body.classList.contains('light')) {
+        body.classList.remove('light');
+        themeToggleBtn.textContent = '☀️ '; 
+    } else {
+        body.classList.add('light');
+        themeToggleBtn.textContent = '🌙 '; 
+    }
+});
