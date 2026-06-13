041fc0dd124a958ed9a4a33f8d3a4730b67c8b60
wahep tamer <mrhosting55555@gmail.com>
2026-06-13 03:05:26 +0300

    update site

--- dark.js
+++ dark.js
@@ -1,26 +1,35 @@
-
-const themeToggleBtn = document.querySelector('.theme-toggle'); 
+const themeToggleBtn = document.querySelector(".theme-toggle");
 const body = document.body;
-themeToggleBtn.addEventListener('click', () => {
-    if (body.classList.contains('light')) {
-        body.classList.remove('light');
-        themeToggleBtn.textContent = '☀️ '; 
-    } else {
-        body.classList.add('light');
-        themeToggleBtn.textContent = '🌙 '; 
-    }
+themeToggleBtn.addEventListener("click", () => {
+  if (body.classList.contains("light")) {
+    body.classList.remove("light");
+    themeToggleBtn.textContent = "☀️ ";
+  } else {
+    body.classList.add("light");
+    themeToggleBtn.textContent = "🌙 ";
+  }
 });
 
-window.addEventListener('load', () => {
-    const preloader = document.getElementById('preloader');
-    if (preloader) {
-        preloader.classList.add('fade-out'); // إخفاء الشاشة بسلاسة
-    }
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  if (preloader) {
+    preloader.classList.add("fade-out"); // إخفاء الشاشة بسلاسة
+  }
 });
 
-const menuBtn = document.getElementById('menuBtn');
-const navLinks = document.getElementById('navLinks');
+// كود فتح وإغلاق القائمة للموبايل وتأثير الأنيميشن للزر
+const mobileMenu = document.getElementById("mobile-menu");
+const navLinks = document.querySelector(".nav-links");
 
-menuBtn.addEventListener('click', () => {
-    navLinks.classList.toggle('open'); // يضيف أو يحذف كلاس open لتظهر القائمة
+mobileMenu.addEventListener("click", () => {
+  navLinks.classList.toggle("active"); // فتح/إغلاق القائمة
+  mobileMenu.classList.toggle("open"); // تحويل الثلاث شرط إلى X وعكسها
+});
+
+// إغلاق القائمة تلقائياً إذا قام المستخدم بالضغط على أي رابط بداخلها
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    mobileMenu.classList.remove("open");
+  });
 });
